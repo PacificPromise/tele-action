8ed7dca3d6e0486e87e7f78ad77ab67bcdf7acdd
DeeTeee <ductuan7575@gmail.com>
2023-06-15 14:50:22 +0700

    test

--- src/notification.sh
+++ src/notification.sh
@@ -1,10 +1,9 @@
 send_telegram() {
-  # curl --location "https://api.telegram.org/bot6178712814:AAESrlahgu1fQ-Mbe-QuAtli6JwQSTJS6vc/sendMessage" \
-  #   --header 'Content-Type: application/json' \
-  #   --data '{
-  #     "chat_id": -1001837632261,
-  #     "reply_to_message_id": 196,
-  #     "text": "'"$1"'"
-  # }'
-  echo $1
+  curl --location "https://api.telegram.org/bot6178712814:AAESrlahgu1fQ-Mbe-QuAtli6JwQSTJS6vc/sendMessage" \
+    --header 'Content-Type: application/json' \
+    --data '{
+      "chat_id": -1001837632261,
+      "reply_to_message_id": 196,
+      "text": "'"$1"'"
+  }'
 }
